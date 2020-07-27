2a55afc95bd8f5e1d1d89424e464bef141ab4f57
Falileeva Maria <raduga.mf@gmail.com>
2020-07-27 15:35:08 +0800

    build

--- src/scripts/preload.js
+++ src/scripts/preload.js
@@ -4,3 +4,7 @@
 const preload = document.querySelector('.preload');
 // preload.classList.remove('preload_hide');
 preload.classList.add('preload_play');
+setTimeout(() => {
+    preload.classList.remove('preload');
+    preload.classList.remove('preload_play');
+}, 2900)
