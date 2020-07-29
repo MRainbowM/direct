5a6f6186d3e137bcd160217a5deb6724b52120cf
Falileeva Maria <raduga.mf@gmail.com>
2020-07-29 18:23:48 +0800

    ашч зфкфддфч

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -15,11 +15,11 @@ window.addEventListener('scroll', (e) => {
         if (isOpacity) {
             card.classList.remove('card_active');
             const img = card.querySelector('.parallax_img');
-            // img.style.transition = '0.3s'
+            img.style.transition = '0.5s'
             img.style.transform = ` rotateY(0deg) rotateX(0deg)`
-            // setTimeout(() => {
-            //     img.style.transition = null
-            // }, 300)
+            setTimeout(() => {
+                img.style.transition = null
+            }, 500)
 
         } else {
             card.classList.add('card_active');
@@ -29,7 +29,7 @@ window.addEventListener('scroll', (e) => {
 window.addEventListener("mousemove", (e) => {
     const card = document.querySelector('.card_active');
     const img = card.querySelector('.parallax_img');
-    let xPos = (e.clientX / img.clientWidth - 0.5) * 10;
-    let yPos = (e.clientY / img.clientHeight - 0.5) * 10;
+    let xPos = (e.clientX / img.clientWidth - 0.5) * 8;
+    let yPos = (e.clientY / img.clientHeight - 0.5) * 8;
     img.style.transform = `rotateY(${yPos}deg) rotateX(${xPos}deg)`
 });
