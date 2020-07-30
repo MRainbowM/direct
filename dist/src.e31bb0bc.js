aac078513c386f85144540def05d7824f153f4fa
Falileeva Maria <raduga.mf@gmail.com>
2020-07-30 16:24:10 +0800

    event hover

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -479,7 +479,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8947" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1334" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
