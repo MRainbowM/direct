3261e41ba3bf3c7d5c7e6b8f689afe0a39cc9444
Falileeva Maria <raduga.mf@gmail.com>
2020-08-04 21:01:15 +0800

    events slider

--- src/scripts/links_drags.js
+++ src/scripts/links_drags.js
@@ -1,36 +1,34 @@
-const clickedEl = document.querySelector('.no-drag');
+const clickedEl = document.querySelectorAll('.no-drag');
 var limit = 5;
 var mouseMoved = false;
 
 function resetEvents() {
-    clickedEl.onmousemove = null;
-    clickedEl.ondragstart = null;
-    clickedEl.onmouseleave = null;
+    clickedEl.forEach(element => {
+        element.onmousemove = null;
+        element.ondragstart = null;
+        element.onmouseleave = null;
+    });
     mouseMoved = false;
 }
-clickedEl.onmousedown = function (downEvent) {
-    if (clickedEl.attributes.href) {
-        clickedEl.onclick = function (clickEvent) {
-            if (mouseMoved) {
-                clickEvent.preventDefault();
-            }
-            resetEvents();
-        };
-    }
-    
-    clickedEl.onmouseleave = function () {
-        resetEvents();
-    };
-
-    clickedEl.onmousemove = function (moveEvent) {
-        clickedEl.ondragstart = function (dragEvent) {
-            dragEvent.preventDefault();
-        };
-
-        if (Math.abs(moveEvent.x - downEvent.x) >= limit || Math.abs(moveEvent.y - downEvent.y) >= limit) {
-            window.getSelection().selectAllChildren(clickedEl);
-            mouseMoved = true;
+clickedEl.forEach(element => {
+    element.onmousedown = (downEvent) => {
+        if (element.attributes.href) {
+            element.onclick = (clickEvent) => {
+                if (mouseMoved) clickEvent.preventDefault();
+                resetEvents();
+            };
         }
-    };
+        element.onmouseleave = () => { resetEvents(); };
 
-};
+        element.onmousemove =  (moveEvent) => {
+            element.ondragstart =  (dragEvent) => {
+                dragEvent.preventDefault();
+            };
+
+            if (Math.abs(moveEvent.x - downEvent.x) >= limit || Math.abs(moveEvent.y - downEvent.y) >= limit) {
+                window.getSelection().selectAllChildren(element);
+                mouseMoved = true;
+            }
+        }
+    }
+})
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,27 @@
 const burger = document.querySelector('.burger')
 const header = document.querySelector('.header')
 const menu = document.querySelector('.menu')
+
+const btnContact = document.querySelector('.menu_btn_contact');
+const menuForm = document.querySelector('.menu_form');
+const menuSoc = document.querySelector('.menu_col_1');
+const menuLinks = document.querySelector('.menu_col_2');
+
+// кнопка в разделе ЛЮДИ: "хочу с вами"
+const btn = document.querySelector('#open-form');
+
+btn.addEventListener('click', (e) => {
+    burger.classList.add('show')
+    header.classList.add('show')
+    menu.classList.add('menu_show')
+    document.body.style.overflow = "hidden"
+    menuSoc.classList.add('menu_col-1_hide');
+    menuLinks.classList.add('menu_col-2_hide');
+    menuForm.classList.remove('menu_form_hide');
+});
+//
+
+
 burger.addEventListener('click', (e) => {
     let isShowMenu = burger.classList.contains('show')
     if (isShowMenu) {
@@ -16,10 +37,7 @@ burger.addEventListener('click', (e) => {
     }
 });
 
-const btnContact = document.querySelector('.menu_btn_contact');
-const menuForm = document.querySelector('.menu_form');
-const menuSoc = document.querySelector('.menu_col_1');
-const menuLinks = document.querySelector('.menu_col_2');
+
 btnContact.addEventListener('click', (e) => {
     menuSoc.classList.add('menu_col-1_hide');
     menuLinks.classList.add('menu_col-2_hide');
