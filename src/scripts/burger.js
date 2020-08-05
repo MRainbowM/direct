e782986f731ec1ace84016988d4ee97ea485a92e
Falileeva Maria <raduga.mf@gmail.com>
2020-08-05 16:39:51 +0800

    burger scroll

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -6,7 +6,9 @@ window.addEventListener('scroll', (e) => {
     const { scrollTop } = document.scrollingElement;
     const lines = burger.querySelectorAll('.line');
     burger.classList.add('burger_animate');
-    if (scrollTop > scrollPoint) {
+    console.log(scrollTop);
+    console.log(scrollPoint);
+    if (scrollTop > scrollPoint && scrollTop > 0) {
         lines.forEach(line => {
             line.style.width = '0';
         });
