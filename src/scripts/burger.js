fea32828f7e35a82be3db7ad5a72449ada92568c
Falileeva Maria <raduga.mf@gmail.com>
2020-08-07 12:15:48 +0800

    burger colors

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,11 +1,33 @@
 const header = document.querySelector('.header');
 const headerLogo = header.querySelector('.logo');
 const burger = header.querySelector('.burger');
+const contacts = document.querySelector('.contacts');
+const footer = document.querySelector('.footer');
 let scrollPoint = 0;
+
 window.addEventListener('scroll', (e) => {
     const { scrollTop } = document.scrollingElement;
     const lines = burger.querySelectorAll('.line');
     burger.classList.add('burger_animate');
+
+    // в футере делаем берый бургер
+    if (scrollTop >= footer.offsetTop) { 
+        lines.forEach(line => {
+            line.style.backgroundColor = 'white';
+        });
+    } else {
+        lines.forEach(line => {
+            line.style.backgroundColor = null;
+        });
+    }
+
+    //в контактах делаем ховер белым
+    if (scrollTop >= contacts.offsetTop && scrollTop <= footer.offsetTop) { 
+        burger.classList.add('hover_white');
+    } else {
+        burger.classList.remove('hover_white');
+    }
+
     if (scrollTop > scrollPoint && scrollTop > 0) {
         lines.forEach(line => {
             line.style.width = '0';
