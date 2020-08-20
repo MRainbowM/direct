2184749d1b9b47899195fec355251b689e772dbd
Falileeva Maria <raduga.mf@gmail.com>
2020-08-20 21:50:23 +0800

    fix phone mask

--- src/scripts/imask.js
+++ src/scripts/imask.js
@@ -2,8 +2,21 @@ import IMask from 'imask';
 
 const iPhones = document.querySelectorAll('.phone');
 const maskOptions = {
-  mask: '+{7}(000)000-00-00'
+  mask: '+{7}(#00)000-00-00',
+  definitions: {
+    '#': /[1,2,3,4,5,6,7,9,0]/
+  }
+  // prepare: function (str) {
+  //   // debugger
+  //   if (str === '8') {
+  //     return '7';
+  //   }
+  //   return str;
+  // }
 };
+
 iPhones.forEach(element => {
-    let mask = IMask(element, maskOptions);
+  let mask = IMask(element, maskOptions);
 });
+
+
