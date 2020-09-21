43c4edcf6372810f6bd2bf3f6a425c6a4b0d27e6
Falileeva Maria <raduga.mf@gmail.com>
2020-09-21 18:06:29 +0800

    projects mobile

--- src/scripts/projects-filters.js
+++ src/scripts/projects-filters.js
@@ -12,16 +12,33 @@ const iso = new Isotope('.cases', {
     // options
 });
 
-const applyFilter = (btns, activName) => {
+const applyFilter = (btns, btns2, activName) => {
     btns.forEach(element => {
         element.addEventListener('click', (e) => {
             const filterValue = element.getAttribute('data-filter');
+            let filterValue2 = '';
+            let actBtn2 = '';
+            if (activName == 'year-active') {
+                btns2.forEach(btn => {
+                    if (btn.classList.contains('filter-active')) {
+                        actBtn2 = btn;
+                    }
+                });
+            } else if (activName == 'filter-active') {
+                btns2.forEach(btn => {
+                    if (btn.classList.contains('year-active')) {
+                        actBtn2 = btn;
+                    }
+                });
+            }
+            filterValue2 = actBtn2.getAttribute('data-filter');
+            const f = filterValue + filterValue2;
             iso.arrange({
-                filter: filterValue
+                filter: f
             });
             element.classList.add(activName);
             btns.forEach(el => {
-                if(element != el) {
+                if (element != el) {
                     el.classList.remove(activName);
                 }
             });
@@ -30,12 +47,13 @@ const applyFilter = (btns, activName) => {
 }
 
 const btnsGroupYears = document.querySelector('.projects-p_years');
-const btnsYears = btnsGroupYears.querySelectorAll('a');
-applyFilter(btnsYears, 'year-active')
+const btnsYears = btnsGroupYears.querySelectorAll('div');
 
 const btnsGroupProj = document.querySelector('.projects-p_filter');
 const btnsProj = btnsGroupProj.querySelectorAll('a');
-applyFilter(btnsProj, 'filter-active')
+
+applyFilter(btnsYears, btnsProj, 'year-active')
+applyFilter(btnsProj, btnsYears, 'filter-active')
 
 const event = new Event("click");
 btnsYears[0].dispatchEvent(event);
