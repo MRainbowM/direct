8310c91b5cd3d1c2e7490a0c39d3ed33e0c0daf5
Falileeva Maria <raduga.mf@gmail.com>
2020-09-21 16:40:53 +0800

    projects filters

--- src/scripts/projects-filters.js
+++ src/scripts/projects-filters.js
@@ -1,39 +1,57 @@
-var Isotope = require('isotope-layout');
+const Isotope = require('isotope-layout');
 
-var elem = document.querySelector('.cases');
+const elem = document.querySelector('.cases');
 
-var iso = new Isotope(elem, {
-    // options
-    itemSelector: '.cases_card',
-    layoutMode: 'fitRows'
-});
-
-// element argument can be a selector string
-//   for an individual element
-var iso = new Isotope('.cases', {
-    // options
-});
-
-// iso.arrange({
-//     // item element provided as argument
-//     filter: function (itemElem) {
-//         // var number = itemElem.querySelector('.year_2020').innerText;
-//         // return parseInt(number, 10) > 50;
-//         // var year = itemElem.getAttribute('data-filter_year');
-//         var number = itemElem.querySelector('.year2020');
-//         return number;
-//     }
+// const iso = new Isotope(elem, {
+//     itemSelector: '.cases_card',
+//     layoutMode: 'fitRows'
 // });
 
-const btnsGroup = document.querySelector('.projects-p_years');
-const btnsYears = btnsGroup.querySelectorAll('a');
 
-btnsYears.forEach(element => {
-    element.addEventListener('click', (e) => {
-        const filterValue = element.getAttribute('data-filter');
-        iso.arrange({
-            filter: filterValue
+const iso = new Isotope('.cases', {
+    // options
+});
+
+const applyFilter = (btns, activName) => {
+    btns.forEach(element => {
+        element.addEventListener('click', (e) => {
+            const filterValue = element.getAttribute('data-filter');
+            iso.arrange({
+                filter: filterValue
+            });
+            element.classList.add(activName);
+            btns.forEach(el => {
+                if(element != el) {
+                    el.classList.remove(activName);
+                }
+            });
         });
     });
-});
+}
+
+const btnsGroupYears = document.querySelector('.projects-p_years');
+const btnsYears = btnsGroupYears.querySelectorAll('a');
+applyFilter(btnsYears, 'year-active')
+// btnsYears.forEach(element => {
+//     element.addEventListener('click', (e) => {
+//         const filterValue = element.getAttribute('data-filter');
+//         iso.arrange({
+//             filter: filterValue
+//         });
+//         element.classList.add('year-active');
+//         btnsYears.forEach(el => {
+//             if(element != el) {
+//                 el.classList.remove('year-active');
+//             }
+//         });
+//     });
+// });
+
+const btnsGroupProj = document.querySelector('.projects-p_filter');
+const btnsProj = btnsGroupProj.querySelectorAll('a');
+applyFilter(btnsProj, 'filter-active')
+
+
+
+
 
