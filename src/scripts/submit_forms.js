12168f848e48a6ec0989857b9b0c072dfe2edf04
Falileeva Maria <raduga.mf@gmail.com>
2020-10-04 19:45:48 +0800

    chenge contacts

--- src/scripts/submit_forms.js
+++ src/scripts/submit_forms.js
@@ -16,23 +16,23 @@ menuForm.addEventListener('submit', (e) => {
     contactPage.classList.add('response_page_show');
 });
 
-if(contactsForm){
-    contactsForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        menuForm.classList.add('menu_form_hide');
-        contactPage.classList.add('response_page_show');
+// if(contactsForm){
+//     contactsForm.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         menuForm.classList.add('menu_form_hide');
+//         contactPage.classList.add('response_page_show');
     
-        menuForm.classList.add('menu_form_hide');
-        burger.classList.add('burger_show');
-        header.classList.add('header_show');
-        document.body.style.overflow = "hidden";
-        document.body.style.marginRight = "7px"
+//         menuForm.classList.add('menu_form_hide');
+//         burger.classList.add('burger_show');
+//         header.classList.add('header_show');
+//         document.body.style.overflow = "hidden";
+//         document.body.style.marginRight = "7px"
     
-        linesBurger.forEach(line => {
-            line.style.width = null;
-        });
-    });
-}
+//         linesBurger.forEach(line => {
+//             line.style.width = null;
+//         });
+//     });
+// }
 
 if(jobForm != null) {
     jobForm.addEventListener('submit', (e) => {
